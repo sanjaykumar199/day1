bdf004e7ef01f8d6e80f5ec37ce3f34462108f3e
sanjay kumar <sanjaykumar14021998@gmail.com>
2022-09-20 09:07:47 -0500

    first commit

---
+++ Massege.java
@@ -0,0 +1,7 @@
+class Massege
+{
+    public static void main (String args[ ] )
+    {
+        System.out.println("hello");
+    }
+}
---
+++ TwoStringEqual.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+class TwoStringEqual {
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String string1 = in.nextLine();
+        System.out.println("Enter the first string: "
+                + string1);
+        String string2 = in.nextLine();
+        System.out.println("Enter the second string :"
+                + string2);
+        System.out.println("\nAre both strings same: ");
+
+        if (string1.equals(string2) == true) {
+            System.out.println("Yes");
+        }
+        else {
+            System.out.println("No");
+        }
+    }
+}
